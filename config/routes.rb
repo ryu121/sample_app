b0053408fff502cc3f9eb09d3c1b62a9ff181164
ryu121 <ryu.engineer.acount@gmail.com>
2019-09-11 04:03:03 +0000

    self save

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+   root 'static_pages#home'
   get '/signup' => 'users#new'
-  root 'static_pages#home'
   get  '/help' => 'static_pages#help'
   get  '/about' => 'static_pages#about'
   get  '/contact' => 'static_pages#contact'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   resources :users
 end
